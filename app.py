d19b8017b44c119253112cb6ebf4b9156a2aa661
anonymousbaniya <santoshbaniya112@gmail.com>
2024-01-18 10:57:58 +0545

    PROECT FILE ADDED

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def student():
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+ 

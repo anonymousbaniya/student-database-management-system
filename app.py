0acda0a811c7c66fa732f6a41fa8cfc9390bb9f2
anonymousbaniya <santoshbaniya112@gmail.com>
2024-01-18 12:14:23 +0545

    A little update on the login, signup pages

--- app.py
+++ app.py
@@ -13,6 +13,14 @@ def student():
     return render_template('login.html')
 
 
+@app.route('/signup')
+def admin():
+    
+    return render_template('signup.html')
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
